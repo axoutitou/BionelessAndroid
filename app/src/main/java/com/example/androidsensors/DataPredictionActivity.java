ec3792aae7a56f96ea93054e66f7b46c0923ea3e
AlphaNam <kevin.rattinam@gmail.com>
2021-03-01 10:04:04 +0100

    Added socket

--- app/src/main/java/com/example/androidsensors/DataPredictionActivity.java
+++ app/src/main/java/com/example/androidsensors/DataPredictionActivity.java
@@ -1,9 +1,11 @@
 package com.example.androidsensors;
 
+import android.annotation.SuppressLint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,8 +20,16 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.Socket;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DataPredictionActivity  extends AppCompatActivity implements SensorEventListener {
 
@@ -29,6 +39,12 @@ public class DataPredictionActivity  extends AppCompatActivity implements Sensor
     private long startTime;
     private AccGyr currentMesure;
     JSONArray jsonArray = new JSONArray();
+
+    private static Socket s;
+    private static PrintWriter printWriter;
+
+    String message ="ON TESTE FORT";
+    private static String ip = "192.168.1.99";
 
 
     @Override
@@ -117,8 +133,9 @@ public class DataPredictionActivity  extends AppCompatActivity implements Sensor
 
         if(floatInstant > 500){
             sendPost(jsonArray.toString());
-            startTime = System.currentTimeMillis();
             jsonArray = new JSONArray();
+            //getMaxOccurenceActivity(jsonArray.toString());
+            startTime = System.currentTimeMillis();
         }
 
     }
@@ -132,6 +149,7 @@ public class DataPredictionActivity  extends AppCompatActivity implements Sensor
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
+
                 try {
                     URL url = new URL("http://8b95afc9-e4d1-4ae6-a2eb-1ec7dc3a8668.westeurope.azurecontainer.io/score");
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -142,7 +160,6 @@ public class DataPredictionActivity  extends AppCompatActivity implements Sensor
                     conn.setDoInput(true);
 
                     DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
                     os.writeBytes(jsonElement);
                     os.flush();
                     os.close();
@@ -160,9 +177,15 @@ public class DataPredictionActivity  extends AppCompatActivity implements Sensor
                         }
                         bufferedReader.close();
 
-                        Log.d("CONTENT", stringBuilder.toString());
+                        getMaxOccurenceActivity(stringBuilder.toString());
+
+                        //Log.d("CONTENT", stringBuilder.toString());
+
+
                     }
                     conn.disconnect();
+
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -175,4 +198,60 @@ public class DataPredictionActivity  extends AppCompatActivity implements Sensor
     public void onStopClick(View view) {
         this.finish();
     }
+
+    public String getMaxOccurenceActivity(final String data){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //data = "[\"Marcher\", \"Marcher\", \"Marcher\", \"Marcher\", \"Marcher\", \"Marcher\", \"Marcher\", \"Marcher\", \"Immobile\", \"Immobile\"]";
+                String tmpData = data.replace("\"","").replace("[","").replace("]","").replace(" ","").replace("\n","").replace("\\","");
+                List<String> myList = new ArrayList<String>(Arrays.asList(tmpData.split(",")));
+
+                int fqDescendre = Collections.frequency(myList,"Descendre");
+                int fqMonter = Collections.frequency(myList,"Monter");
+                int fqCourir = Collections.frequency(myList,"Courir");
+                int fqMarcher = Collections.frequency(myList,"Marcher");
+                int fqImmobile = Collections.frequency(myList,"Immobile");
+
+                ArrayList<Integer> listFq = new ArrayList<Integer>();
+                listFq.add(fqDescendre);
+                listFq.add(fqMonter);
+                listFq.add(fqCourir);
+                listFq.add(fqMarcher);
+                listFq.add(fqImmobile);
+
+                HashMap<String,Integer> mapFq = new HashMap<String,Integer>();
+                mapFq.put("Descendre",fqDescendre);
+                mapFq.put("Monter",fqMonter);
+                mapFq.put("Courir",fqCourir);
+                mapFq.put("Marcher",fqMarcher);
+                mapFq.put("Immobile",fqImmobile);
+
+                int max = Collections.max(listFq);
+
+                @SuppressLint({"NewApi", "LocalSuppress"}) String maxMouvement = Collections.max(mapFq.entrySet(), Map.Entry.comparingByValue()).getKey().toString();
+                Log.d("MAAAAAAXXX", String.valueOf(max));
+
+                try {
+                    s = new Socket(ip, 5000);
+                    printWriter = new PrintWriter(s.getOutputStream());
+                    printWriter.write(maxMouvement);
+                    printWriter.flush();
+                    printWriter.close();
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        thread.start();
+
+
+        return null;
+
+    }
+
+
 }
